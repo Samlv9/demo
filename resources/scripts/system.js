71c09f55e489f42289188eed77370a00a9f363b1
Samlv9 <samlv9@live.com>
2015-12-10 16:43:27 +0800

    scroll container test

---
+++ resources/scripts/system.js
@@ -0,0 +1,2 @@
+(function(domain,undefined){"use strict";document.addEventListener("touchmove",function(t){t.preventDefault()}),document.addEventListener("touchstart",function(t){t.preventDefault()});if(typeof Page == "function" && !document.currentPage){domain.currentPage=new Page();}}(this||window));
+//# sourceMappingURL=system.js.map
